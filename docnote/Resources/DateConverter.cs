f95c1bd3d75a016c1e3ff8c7e4d41dbc99788f4b
Zuiko Stanislav <zuiko@outlook.com>
2016-09-06 22:20:21 +0300

    DateConverter

---
+++ docnote/Resources/DateConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace docnote.Resources
+{
+    public class DateConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            DateTime date;
+            if (value != null && DateTime.TryParse(value.ToString(), out date))
+            {
+                return date;
+            }
+
+            return null;
+        }
+    }
+}
